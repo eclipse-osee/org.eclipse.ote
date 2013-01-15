0b451335db8a63733c468c1635fbea58c59bb5ca
Andrew M Finkbeiner <andrew.m.finkbeiner@boeing.com>
2013-01-15 16:45:46 -0700

    feature: Create org.eclipse.osee.ote.ui.mux

---
+++ plugins/org.eclipse.osee.ote.ui.mux/src/org/eclipse/osee/ote/ui/mux/OteMuxImage.java
@@ -0,0 +1,33 @@
+/*
+ * Created on Jun 12, 2009
+ *
+ * PLACE_YOUR_DISTRIBUTION_STATEMENT_RIGHT_HERE
+ */
+package org.eclipse.osee.ote.ui.mux;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.osee.framework.ui.swt.ImageManager;
+import org.eclipse.osee.framework.ui.swt.KeyedImage;
+
+/**
+ * @author b1528444
+ */
+public enum OteMuxImage implements KeyedImage {
+   MUX("1553.gif");
+
+   private final String fileName;
+
+   private OteMuxImage(String fileName) {
+      this.fileName = fileName;
+   }
+
+   @Override
+   public ImageDescriptor createImageDescriptor() {
+      return ImageManager.createImageDescriptor(MuxToolPlugin.PLUGIN_ID, "icons", fileName);
+   }
+
+   @Override
+   public String getImageKey() {
+      return MuxToolPlugin.PLUGIN_ID + ".icons." + fileName;
+   }
+}
