a7b1144d60118be5ba1eb5d61b941cabbe7f91f5
David Phillips <david.n.phillips@boeing.com>
2013-09-19 11:41:56 -0700

    refinement: all elements are not added for messages input from search view

--- org.eclipse.ote.ui.message/src/org/eclipse/ote/ui/message/MessageInputComponent.java
+++ org.eclipse.ote.ui.message/src/org/eclipse/ote/ui/message/MessageInputComponent.java
@@ -25,35 +25,33 @@ import org.eclipse.ui.PlatformUI;
 
 public class MessageInputComponent implements MessageInput {
 
-	@Override
-	public String getLabel() {
-		return "Message Watch";
-	}
+   @Override
+   public String getLabel() {
+      return "Message Watch";
+   }
 
-	@Override
-	public void add(List<MessageInputItem> items) {
-		AddWatchParameter watchParameter = new AddWatchParameter();
-		recursiveAdd(watchParameter, items);
-		try {
-			final IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-			WatchView watchView;
-			watchView = (WatchView) page.showView(WatchView.VIEW_ID);
-			watchView.addWatchMessage(watchParameter);
-		} catch (PartInitException e) {
-			OseeLog.log(getClass(), Level.SEVERE, "Unable to add messages to MessageWatch", e);
-		}
-	}
-	
-	private void recursiveAdd(AddWatchParameter watchParameter, List<MessageInputItem> items){
-		for(MessageInputItem item:items){
-			Object[] obj = item.getElementPath();
-			if(obj != null){
-				watchParameter.addMessage(item.getMessageClass(), new ElementPath(obj));
-			} else {
-				watchParameter.addMessage(item.getMessageClass());
-			}
-			recursiveAdd(watchParameter, item.getChildren());
-		}
-	}
+   @Override
+   public void add(List<MessageInputItem> items) {
+      AddWatchParameter watchParameter = new AddWatchParameter();
+      add(watchParameter, items);
+      try {
+         final IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+         WatchView watchView = (WatchView) page.showView(WatchView.VIEW_ID);
+         watchView.addWatchMessage(watchParameter);
+      } catch (PartInitException e) {
+         OseeLog.log(getClass(), Level.SEVERE, "Unable to add messages to MessageWatch", e);
+      }
+   }
+
+   private void add(AddWatchParameter watchParameter, List<MessageInputItem> items){
+      for(MessageInputItem item:items){
+         Object[] obj = item.getElementPath();
+         if(obj != null){
+            watchParameter.addMessage(item.getMessageClass(), new ElementPath(obj));
+         } else {
+            watchParameter.addMessage(item.getMessageClass());
+         }
+      }
+   }
 
 }
