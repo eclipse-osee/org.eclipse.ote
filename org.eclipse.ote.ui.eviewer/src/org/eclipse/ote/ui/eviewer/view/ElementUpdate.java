b31f4dc886b68cf8e5479d023b1879720cb35cc8
kaguilar <ken.aguilar@gmail.com>
2014-04-07 09:43:30 -0700

    bug: fix eception while streaming to a csv file

--- org.eclipse.ote.ui.eviewer/src/org/eclipse/ote/ui/eviewer/view/ElementUpdate.java
+++ org.eclipse.ote.ui.eviewer/src/org/eclipse/ote/ui/eviewer/view/ElementUpdate.java
@@ -88,6 +88,9 @@ public final class ElementUpdate {
 
    public Object getValue(ViewerColumn column) {
       Integer index = valueMap.get(column);
+      if (index >= values.length) {
+    	  return "invalid index " + index;
+      }
       return index != null ? values[index] : null;
    }
 
