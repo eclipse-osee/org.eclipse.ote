77a74cefd7eb72d3d5bfaefabf00d421852f4a43
Andy Jury <andy.jury@boeing.com>
2019-06-25 16:00:51 -0700

    feature: Initial commit of test server product bundles.
    Change-Id: I27c43bac67745abe383fd00b5a67aa5baa94cb69

---
+++ org.eclipse.ote.test.server.parent/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo
+export BASE_AREA=`pwd | sed 's/\/c\//\/c\:\//'`
+echo "mvn clean verify -Dosee-build-stage=nothing -Dosee.base.p2=$1 -Dosee.ote.p2=file://$BASE_AREA//../../org.eclipse.osee.ote.p2/target/repository -Dno-extern-repos"
+mvn clean verify -Dosee-build-stage=nothing -Dosee.base.p2=$1 -Dosee.ote.p2=file://$BASE_AREA/../org.eclipse.osee.ote.p2/target/repository -Dno-extern-repos
