5c95d4ef88f547123846bb8b669d4344ff28a965
Dominic Guss <Dominic.Guss@boeing.com>
2020-07-22 09:07:28 -0700

    feature: Force Save Actions on resource change
    Change-Id: Ib82a7bbf8227725071188dd8006ef9311a24379d

--- org.eclipse.ote.simple.oteide.product.load/src/org/eclipse/ote/simple/oteide/product/load/LoadPreferences.java
+++ org.eclipse.ote.simple.oteide.product.load/src/org/eclipse/ote/simple/oteide/product/load/LoadPreferences.java
@@ -18,10 +18,14 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
-
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.core.runtime.preferences.ConfigurationScope;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.core.runtime.preferences.IExportedPreferences;
 import org.eclipse.core.runtime.preferences.IPreferenceFilter;
 import org.eclipse.core.runtime.preferences.IPreferencesService;
@@ -31,50 +35,63 @@ import org.eclipse.osee.framework.jdk.core.type.OseeCoreException;
 import org.eclipse.ui.IStartup;
 
 /**
- * Loads a custom Eclipse .epf preferences file on startup.
+ * Loads a custom Eclipse .epf preferences file on startup. Forces Save Actions preferences on startup or when a new
+ * workspace has been created.
  * 
  * @author Dominic Guss
  */
 public class LoadPreferences implements IStartup {
+   private final String PREFERENCES_FILE_PATH =
+      System.getProperty("user.dir") + File.separator + "OSEE_Java_Preferences.epf";
 
-	private final String PREFERENCES_FILE_PATH = System.getProperty("user.dir") + File.separator
-			+ "OSEE_Java_Preferences.epf";
+   Job saveActionsJob = new Job("Save Actions Preferences Job") {
+      @Override
+      public IStatus run(IProgressMonitor monitor) {
+         IEclipsePreferences ePrefs = InstanceScope.INSTANCE.getNode("org.eclipse.jdt.ui");
+         ePrefs.putBoolean("editor_save_participant_org.eclipse.jdt.ui.postsavelistener.cleanup", true);
+         ePrefs.putBoolean("sp_cleanup.format_source_code", true);
+         ePrefs.putBoolean("sp_cleanup.organize_imports", true);
+         ePrefs.putBoolean("sp_cleanup.on_save_use_additional_actions", true);
+         return Status.OK_STATUS;
+      }
+   };
 
-	@Override
-	public void earlyStartup() {
+   @Override
+   public void earlyStartup() {
+      saveActionsJob.schedule();
 
-		IPreferenceFilter[] ipFilters = new IPreferenceFilter[1];
+      IPreferenceFilter[] ipFilters = new IPreferenceFilter[1];
 
-		ipFilters[0] = new IPreferenceFilter() {
-			@Override
-			public String[] getScopes() {
-				return new String[] { InstanceScope.SCOPE, ConfigurationScope.SCOPE };
-			}
+      ipFilters[0] = new IPreferenceFilter() {
+         @Override
+         public String[] getScopes() {
+            return new String[] {InstanceScope.SCOPE, ConfigurationScope.SCOPE};
+         }
 
-			@Override
-			public Map<String, PreferenceFilterEntry[]> getMapping(String scope) {
-				return null;
-			}
-		};
+         @Override
+         public Map<String, PreferenceFilterEntry[]> getMapping(String scope) {
+            return null;
+         }
+      };
 
-		IPreferencesService service = Platform.getPreferencesService();
-		InputStream inputStream = null;
-		IExportedPreferences prefs = null;
+      IPreferencesService service = Platform.getPreferencesService();
+      InputStream inputStream = null;
+      IExportedPreferences prefs = null;
 
-		try {
-			inputStream = new FileInputStream(new File(PREFERENCES_FILE_PATH));
-		} catch (IOException ex) {
-			OseeCoreException.wrap(ex);
-		}
-		try {
-			prefs = service.readPreferences(inputStream);
-		} catch (CoreException ex) {
-			OseeCoreException.wrap(ex);
-		}
-		try {
-			service.applyPreferences(prefs, ipFilters);
-		} catch (CoreException ex) {
-			OseeCoreException.wrap(ex);
-		}
-	}
+      try {
+         inputStream = new FileInputStream(new File(PREFERENCES_FILE_PATH));
+      } catch (IOException ex) {
+         OseeCoreException.wrap(ex);
+      }
+      try {
+         prefs = service.readPreferences(inputStream);
+      } catch (CoreException ex) {
+         OseeCoreException.wrap(ex);
+      }
+      try {
+         service.applyPreferences(prefs, ipFilters);
+      } catch (CoreException ex) {
+         OseeCoreException.wrap(ex);
+      }
+   }
 }
