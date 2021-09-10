06a02fc21320c9a5344716e11cb8a4424b2d3ddb
Michael P. Masterson <michael.p.masterson@boeing.com>
2021-09-10 14:30:16 -0700

    feature: Removing direct plugin references when possible
    Preferring feature inclusion over plugins where applicable.
    
    Change-Id: Iafdd48ca2b14a61a363515d70a9aea83c9aebe64

--- org.eclipse.ote.build/dependencies/ote.dependencies.parent/build.sh
+++ org.eclipse.ote.build/dependencies/ote.dependencies.parent/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo
 CLIENT_SITE="http://ci.eclipse.org/osee/job/osee_dev_0.26.0/lastSuccessfulBuild/artifact/org.eclipse.osee/plugins/org.eclipse.osee.client.all.p2/target/repository/"
-SERVER_STIE="http://ci.eclipse.org/osee/job/osee_dev_0.26.0/lastSuccessfulBuild/artifact/org.eclipse.osee/plugins/org.eclipse.osee.server.p2/target/repository/"
+SERVER_SITE="http://ci.eclipse.org/osee/job/osee_dev_0.26.0/lastSuccessfulBuild/artifact/org.eclipse.osee/plugins/org.eclipse.osee.server.p2/target/repository/"
 DEP_SITE="http://ci.eclipse.org/osee/view/OSEE_Developer/job/osee_dev_0.26.0/lastSuccessfulBuild/artifact/org.eclipse.osee/plugins/org.eclipse.osee.dep.p2/target/repository/"
 
 echo "mvn clean verify -P-mirror-other-sites -V -B -Dosee-client-site=${CLIENT_SITE} -Dosee-server-site=${SERVER_SITE} -Dosee-dep-site=${DEP_SITE}"
