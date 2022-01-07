fac1b7cf07fc97880f74b83b1ea1d294660ed6a9
Andy Jury <andy.jury@boeing.com>
2022-01-07 16:13:32 -0700

    feature: Update to build with OSEE Dev
    Change-Id: Ibfead7a041efdae580557ec3edfd8efef626a6d9

--- org.eclipse.osee.ote.parent/build.sh
+++ org.eclipse.osee.ote.parent/build.sh
@@ -2,6 +2,6 @@
 echo
 pwd
 echo
-echo "mvn clean verify -Dosee.base.p2=$1 -Dosee-build-stage=nothing -Dno-extern-repos"
+echo "mvn clean verify -Dote.dependencies.p2=$1 -Dosee-build-stage=nothing -Dno-extern-repos"
 echo
-mvn clean verify -Dosee.base.p2=$1 -Dosee-build-stage=nothing -Dno-extern-repos
+mvn clean verify -Dote.dependencies.p2=$1 -Dosee-build-stage=nothing -Dno-extern-repos
--- org.eclipse.osee.ote.parent/buildLocal.sh
+++ org.eclipse.osee.ote.parent/buildLocal.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export BASE_AREA=`pwd | sed 's/\/c\//\/c\:\//'`
-echo "mvn clean verify -Dosee.base.p2=file://$BASE_AREA/../org.eclipse.ote.build/dependencies/ote.dependencies.p2/target/repository/" 
-mvn clean verify -Dosee.base.p2=file://$BASE_AREA/../org.eclipse.ote.build/dependencies/ote.dependencies.p2/target/repository/ 
+echo "mvn clean verify -Dote.dependencies.p2=file://$BASE_AREA/../org.eclipse.ote.build/dependencies/ote.dependencies.p2/target/repository/" 
+mvn clean verify -Dote.dependencies.p2=file://$BASE_AREA/../org.eclipse.ote.build/dependencies/ote.dependencies.p2/target/repository/ 
