81b84b70f0d25744147e436cf8bf801bc543ff95
Andy Jury <andy.jury@boeing.com>
2022-06-21 11:42:14 -0700

    feature: Update buildLocal.sh
    Change-Id: I417e3940d852e712aec08d3a00f981dff6eebf11

--- org.eclipse.osee.ote.parent/buildLocal.sh
+++ org.eclipse.osee.ote.parent/buildLocal.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export BASE_AREA=`pwd | sed 's/\/c\//\/c\:\//'`
-echo "mvn clean verify -Dote.dependencies.p2=file://$BASE_AREA/../org.eclipse.ote.build/dependencies/ote.dependencies.p2/target/repository/" 
-mvn clean verify -Dote.dependencies.p2=file://$BASE_AREA/../org.eclipse.ote.build/dependencies/ote.dependencies.p2/target/repository/ 
+echo "mvn clean verify -Dote.dependencies.p2=file://$BASE_AREA/../org.eclipse.ote.build/dependencies/ote.dependencies.p2/target/repository/ -Dosee-build-stage=nothing -Dno-extern-repos" 
+mvn clean verify -Dote.dependencies.p2=file://$BASE_AREA/../org.eclipse.ote.build/dependencies/ote.dependencies.p2/target/repository/ -Dosee-build-stage=nothing -Dno-extern-repos 
