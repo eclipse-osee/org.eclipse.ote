d6af35fd3181f55d4e24dfa3ded0fe804d6518d4
Dominic Leiner <dominic.j.leiner@boeing.com>
2023-08-11 13:14:10 -0700

    feature: cleanup
    Change-Id: I3a8d1a94c146893af67f6e2a9b0afdea08f6ab12

--- org.eclipse.ote.parent/buildLocal.sh
+++ org.eclipse.ote.parent/buildLocal.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 export BASE_AREA=`pwd | sed 's/\/c\//\/c\:\//'`
 echo "mvn clean verify -Dote.dependencies.p2=file://$BASE_AREA/../org.eclipse.ote.build/dependencies/ote.dependencies.p2/target/repository/ -Dosee.ote.p2=file://$BASE_AREA/../org.eclipse.osee.ote.p2/target/repository -Dosee-build-stage=nothing -Dno-extern-repos" 
-JAVA_HOME="/c/Tools/jdk-11.0.7/jdk-11.0.7"
 mvn clean verify -Dote.dependencies.p2=file://$BASE_AREA/../org.eclipse.ote.build/dependencies/ote.dependencies.p2/target/repository/ -Dosee.ote.p2=file://$BASE_AREA/../org.eclipse.osee.ote.p2/target/repository -Dosee-build-stage=nothing -Dno-extern-repos 
