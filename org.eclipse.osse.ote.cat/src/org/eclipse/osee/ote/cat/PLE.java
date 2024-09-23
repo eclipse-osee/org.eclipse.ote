2150b516686b6b80d0448b547ea59660f1b5deed
loren.k.ashley <loren.k.ashley@boeing.com>
2024-09-23 12:05:06 -0700

    feature[F1812462]: Create PLE Demo Test Script
    Adds PLE annotation definition for Java Test Script classes. This annotation will
    be used to trigger the Compiler Applicability Tool annotation processor to process
    the PLE annotated Java Test Script class with the BAT for each Product Configuration
    in the selected PLE Configuration.
    
    Change-Id: I9a984517d2e49997dbe356768dc99576531ddc50

---
+++ org.eclipse.osse.ote.cat/src/org/eclipse/osee/ote/cat/PLE.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.osee.ote.cat;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * A class annotation to indicate to the Compiler Applicability Tool (CAT) that the annotated class is to be processed
+ * for <i>applicabilityStatements</i> and a version of the annotated class is to be created for each Product
+ * Configuration in the selected Product Line Engineering (PLE) Configuration.
+ * 
+ * @author Loren K. Ashley
+ */
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PLE {
+}
